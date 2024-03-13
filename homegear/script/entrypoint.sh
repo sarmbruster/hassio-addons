5d647f24dd72236b7f0888873e1e6b7cdd0607de
Stefan Armbruster <stefan@armbruster-it.de>
2024-03-13 20:48:31 +0100

    basic working version

--- homegear/script/entrypoint.sh
+++ homegear/script/entrypoint.sh
@@ -1,10 +1,101 @@
 #/bin/bash
 
+_term() {
+	HOMEGEAR_PID=$(cat /var/run/homegear/homegear.pid)
+	# kill $(cat /var/run/homegear/homegear-management.pid)
+	# kill $(cat /var/run/homegear/homegear-webssh.pid)
+	# kill $(cat /var/run/homegear/homegear-influxdb.pid)
+	kill $HOMEGEAR_PID
+	wait "$HOMEGEAR_PID"
+	/etc/homegear/homegear-stop.sh
+	exit 0
+}
+
+trap _term SIGTERM
+
+if [[ $GET_VERSION -eq 1 ]]; then
+	homegear -v
+	exit $?
+fi
+
+USER=homegear
+
+USER_ID=$(id -u $USER)
+USER_GID=$(id -g $USER)
+
+USER_ID=${HOST_USER_ID:=$USER_ID}
+USER_GID=${HOST_USER_GID:=$USER_GID}
+
+if [ $USER_ID -ne 0 ]; then
+	sed -i -e "s/^${USER}:\([^:]*\):[0-9]*:[0-9]*/${USER}:\1:${USER_ID}:${USER_GID}/" /etc/passwd
+	sed -i -e "s/^${USER}:\([^:]*\):[0-9]*/${USER}:\1:${USER_GID}/" /etc/group
+fi
+
 if [ ! -d /config/homegear ]; then
-  cp -r /etc/homegear /config
+    mv /etc/homegear /config
 fi
 rm -rf /etc/homegear
-ln -s /config/homegear /etc
 
-sleep 10000000
-#/start.sh
+ln -snf /config/homegear /etc/homegear
+
+if test ! -e /etc/homegear/nodeBlueCredentialKey.txt; then
+        tr -dc A-Za-z0-9 < /dev/urandom | head -c 43 > /etc/homegear/nodeBlueCredentialKey.txt
+        chmod 400 /etc/homegear/nodeBlueCredentialKey.txt
+fi
+
+rm -f /var/lib/homegear/homegear_updated
+
+if ! [ -f /var/log/homegear/homegear.log ]; then
+	touch /var/log/homegear/homegear.log
+	# touch /var/log/homegear/homegear-webssh.log
+	# touch /var/log/homegear/homegear-flows.log
+	# touch /var/log/homegear/homegear-scriptengine.log
+	# touch /var/log/homegear/homegear-management.log
+	# touch /var/log/homegear/homegear-influxdb.log
+fi
+
+if ! [ -f /etc/homegear/dh1024.pem ]; then
+	openssl genrsa -out /etc/homegear/homegear.key 2048
+	openssl req -batch -new -key /etc/homegear/homegear.key -out /etc/homegear/homegear.csr
+	openssl x509 -req -in /etc/homegear/homegear.csr -signkey /etc/homegear/homegear.key -out /etc/homegear/homegear.crt
+	rm /etc/homegear/homegear.csr
+	chmod 400 /etc/homegear/homegear.key
+	openssl dhparam -check -text -5 -out /etc/homegear/dh1024.pem 1024
+	chmod 400 /etc/homegear/dh1024.pem
+fi
+
+chown -R root:root /etc/homegear
+chown ${USER}:${USER} /etc/homegear/*.key
+chown ${USER}:${USER} /etc/homegear/*.pem
+chown ${USER}:${USER} /etc/homegear/nodeBlueCredentialKey.txt
+chown ${USER}:${USER} /etc/homegear/ca/private/*.key
+find /etc/homegear -type d -exec chmod 755 {} \;
+chown -R ${USER}:${USER} /var/log/homegear /var/lib/homegear
+find /var/log/homegear -type d -exec chmod 750 {} \;
+find /var/log/homegear -type f -exec chmod 640 {} \;
+find /var/lib/homegear -type d -exec chmod 750 {} \;
+find /var/lib/homegear -type f -exec chmod 640 {} \;
+find /var/lib/homegear/scripts -type f -exec chmod 550 {} \;
+
+TZ=$(echo $TZ | tr -d '"') # Some users report quotes around the string - remove them
+if [[ -n $TZ ]]; then
+	ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+fi
+
+mkdir -p /var/run/homegear
+chown ${USER}:${USER} /var/run/homegear
+
+/etc/homegear/homegear-start.sh
+/usr/bin/homegear -u ${USER} -g ${USER} -p /var/run/homegear/homegear.pid &
+#sleep 5
+# /usr/bin/homegear-management -p /var/run/homegear/homegear-management.pid &
+# /usr/bin/homegear-webssh -p /var/run/homegear/homegear-webssh.pid &
+# /usr/bin/homegear-influxdb -u ${USER} -g ${USER} -p /var/run/homegear/homegear-influxdb.pid &
+# tail -f /var/log/homegear/homegear-webssh.log &
+# tail -f /var/log/homegear/homegear-flows.log &
+# tail -f /var/log/homegear/homegear-scriptengine.log &
+# tail -f /var/log/homegear/homegear-management.log &
+# tail -f /var/log/homegear/homegear-influxdb.log &
+tail -f /var/log/homegear/homegear.log &
+child=$!
+wait "$child"
